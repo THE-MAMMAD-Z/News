619420c044b43c8f0810f22852808893d856bfaa
mohammad <shir34@yahoo.com>
2024-07-20 02:00:52 +0430

    add workflow

--- core/settings.py
+++ core/settings.py
@@ -96,8 +96,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME', 'khabar'),
+        'USER': os.environ.get('DB_USER', 'majid'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'Sezar456789'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
+        'TEST': {
+            'NAME': 'test_khabar',  # The test database name
+        },
     }
 }
 
